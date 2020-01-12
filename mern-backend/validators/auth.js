0c6e1cd1f4cf699cdc513f91b2e9b3b370ccce3f
Jokūbas Toločka <jokubas.tj@gmail.com>
2020-01-12 18:00:06 +0200

    forgot password and reset password functions

--- mern-backend/validators/auth.js
+++ mern-backend/validators/auth.js
@@ -21,3 +21,19 @@ exports.userSigninValidator = [
         .isLength({min: 6})
         .withMessage('Must be at least 6 characters long'),
 ]
+
+exports.forgotPasswordValidator = [
+    check('email')
+        .not()
+        .isEmpty()
+        .isEmail()
+        .withMessage('Must be a valid Email adress'),
+]
+
+exports.resetPasswordValidator = [
+    check('newPassword')
+        .not()
+        .isEmpty()
+        .isLength({min: 6})
+        .withMessage('Must be at least 6 characters long'),
+]
