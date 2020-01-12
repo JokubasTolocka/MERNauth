73368f4603dc913f342494715345f08b845c0902
Jokūbas Toločka <jokubas.tj@gmail.com>
2020-01-12 20:15:08 +0200

    forgot password page

---
+++ mern-frontend/src/containers/Forgot.js
@@ -0,0 +1,61 @@
+import React, {useState} from 'react'
+import Layout from './Layout';
+import axios from 'axios';
+import {ToastContainer, toast} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.min.css';
+
+const Forgot = ({history}) => {
+    const [values, setValues] = useState({
+        email: '',
+        buttonText: 'Request'
+    });
+    const { email, buttonText} = values;
+
+    const handleChange = (name) => (event) => {
+        setValues({...values, [name]: event.target.value})
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        setValues({...values, buttonText: 'Submitting...'});
+        axios({
+            method: 'PUT',
+            url: `${process.env.REACT_APP_API}/forgot-password`,
+            data: { email}
+        })
+        .then(res => {
+            toast.success(response.data.message)
+            setValues({...values, buttonText: 'Requested'})
+        })
+        .catch(err => {
+            setValues({...values, buttonText: 'Request'});
+            toast.error(err.response.data.error);
+        })
+    }
+
+    const passwordForgotForm = () => (
+        <form>
+            <div>
+                <label htmlFor='email'>Email</label>
+                <input
+                type='email'
+                onChange={handleChange('email')}
+                value={email}
+                />
+            </div>
+            <div>
+                <button onClick={handleSubmit}>{buttonText}</button>
+            </div>
+        </form>
+    )
+
+    return(
+    <Layout>
+        <ToastContainer/>
+        <h1>Forgot Password</h1>
+        {passwordForgotForm()}
+    </Layout>
+    )
+}
+
+export default Forgot;
--- mern-frontend/src/containers/Main.js
+++ mern-frontend/src/containers/Main.js
@@ -8,6 +8,7 @@ import Private from './Private';
 import PrivateRoute from './PrivateMain';
 import AdminRoute from './AdminRoute';
 import Admin from './Admin';
+import Forgot from './Forgot';
 
 
 const Routes = () => {
@@ -16,6 +17,7 @@ const Routes = () => {
             <Switch>
                 <Route exact path='/' component={App}/>
                 <Route exact path='/signup' component={Signup}/>
+                <Route exact path='/auth/password/forgot' component={Forgot}/>
                 <Route exact path='/signin' component={Signin}/>
                 <Route exact path='/auth/activate/:token' component={Activate}/>
                 <PrivateRoute exact path='/private' component={Private}/>
